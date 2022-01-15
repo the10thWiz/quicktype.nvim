e15266b9292325db9016020d0078e0af2e4edfec
Matthew Pomes <the10thwiz@gmail.com>
2022-01-14 19:31:17 -0600

    Add hidden files

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,6 @@ module.exports = {
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { 'vars': 'all', 'args': 'after-used', 'ignoreRestSiblings': false, 'varsIgnorePattern': '^_'}]
   },
 };
